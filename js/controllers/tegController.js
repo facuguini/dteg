b2b38e530b50a01309d989e2addffbba8e83a836
gonpombo <gpombo@despegar.com>
2014-09-11 10:41:10 -0300

    sonidos

--- js/controllers/tegController.js
+++ js/controllers/tegController.js
@@ -85,11 +85,11 @@ app.controller('TegController', ['$scope', function($scope) {
         setTimeout(function(){ setFalse() }, 2000);
       } else if(($scope.cantAt == 3) && ($scope.cantDe == 3) && ($scope.sacaAta == 3)) {
        	//document.getElementById(sounds[random(0,2)]).play();
-       	//var med = new Media("/www/sounds/perdiste.mp3").play();
+       	var med = new Media("/www/sounds/perdiste.mp3").play();
        	var med1 = new Media("/android_asset/www/sounds/"+sounds[random(0,2)]+".mp3").play();
-       	//var med2 = new Media("/sounds/perdiste.mp3").play();
-       	//var med3 = new Media("perdiste.mp3").play();
-       	//var med4 = new Media("/android/www/sounds/perdiste.mp3").play();
+       	var med2 = new Media("/sounds/perdiste.mp3").play();
+       	var med3 = new Media("perdiste.mp3").play();
+       	var med4 = new Media("/android/www/sounds/perdiste.mp3").play();
        	$scope.btnDisabled = true;
         setTimeout(function(){ btnEnable() }, 2000);
 
